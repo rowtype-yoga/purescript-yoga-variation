70d1635142621169123f4a53e52df7f5d82debfd
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2022-06-03 17:39:14 +0100

    Initial commit

---
+++ src/Yoga/Variation/Variation.purs
@@ -0,0 +1,28 @@
+module Yoga.Variation.Variation where
+
+import Prelude
+
+import Data.Array.NonEmpty (NonEmptyArray)
+import Data.Array.NonEmpty as NonEmpty
+import Data.Symbol (class IsSymbol)
+import Data.Validation.Semigroup as Validation
+import Data.Variant as Variant
+import Prim.Row (class Cons)
+import Type.Proxy (Proxy(..))
+
+type Validated err result = Validation.V (NonEmptyArray (Variant.Variant err)) result
+
+valid :: forall err result. result -> Validated err result
+valid = pure
+
+invalid
+  ∷ ∀ (t63 ∷ Type) (sym ∷ Symbol) (error ∷ Type) (others ∷ Row Type) (errors ∷ Row Type)
+  . Cons sym error others errors
+  ⇒ IsSymbol sym
+  ⇒ Proxy sym
+  → error
+  → Validation.V (NonEmptyArray (Variant.Variant errors)) t63
+invalid proxy value = Validation.invalid $ NonEmpty.singleton $ Variant.inj proxy value
+
+key ∷ ∀ (sym ∷ Symbol). Proxy sym
+key = Proxy
